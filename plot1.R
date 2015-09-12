955e33285648e4750db84bee52cac075d87ec464
Juan R Musumeci <jrmusumeci@live.com>
2015-09-12 11:42:48 -0400

    Project 1

--- plot1.R
+++ plot1.R
@@ -8,5 +8,5 @@ data <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";",
 subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 png("plot1.png", width=480, height=480)
-hist(subData$Global_active_power,col="#ff3500",xlab="Global Active Power (kilowats)",main="Global Active Power")
+hist(subData$Global_active_power,col="#ff2300",xlab="Global Active Power (kilowats)",main="Global Active Power")
 dev.off()
